756b952f0a34489f3e83936679134c81064c48d0
najmaA <najmaamin10@gmai.com>
2020-01-24 11:10:52 +0300

    password and username credentials added

--- credential_test.py
+++ credential_test.py
@@ -28,8 +28,9 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),0)
         
-        class TestCredential(unittest.TestCase):
-        '''
+        
+class TestCredential(unittest.TestCase):
+    '''
     Test class that defines teself.3+st cases for the credentials class behaviours.
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
@@ -47,3 +48,77 @@ class TestUser(unittest.TestCase):
                 current_user = user.lastname
         return current_user
         self.assertEqual(current_user, Credential.confirm_login(user2.password, user2.lastname))
+        
+    def setUp(self):
+        '''
+        Function to create an account's credentials before each test
+        '''
+        self.new_credential = Credential('twitter','najma','12345abcd')
+        
+    def test__init__(self):
+        '''
+        test__init__ test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_credential.account_name,'twitter')
+        self.assertEqual(self.new_credential.username,'najma')
+        self.assertEqual(self.new_credential.password,'12345abcd')
+        
+    def test_save_credential(self):
+        '''
+        Test to confirm if the new credential is saved to the credentials list
+        '''
+        self.new_credential.save_credential()
+        instagram = Credential('Instagram','najma','12345abcd')
+        instagram.save_credential()
+        self.assertEqual(len(Credential.credential_list),9)
+        
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Credential.Czredential_list = []
+        User.user_list = []
+        
+    def test_display_credential(self):
+        '''
+        Test to check if credentials display method displays
+        '''
+        self.new_credential.save_credential()
+        instagram = Credential('Instagram','najma','1234abcd')
+        instagram.save_credential()
+        facebook = Credential('Facebook','najma','1234abcd')
+        facebook.save_credential()
+        self.assertEqual(len(Credential.display_credential(instagram.username)), 9)
+        
+    def test_find_by_account_name(self):
+        '''
+        Test method for finding a credential site_name
+        '''
+        self.new_credential.save_credential()
+        instagram = Credential('Instagram','najma','1234abcd')
+        instagram.save_credential()
+        credential_exist = Credential.find_by_account_name('Instagram')
+        # self.assertEqual(credential_exists, instagram)
+        
+        
+    def test_copy_credential(self):
+        '''
+        Test method for copy credential functionality
+        '''
+        self.new_credential.save_credential()
+        instagram = Credential('Instagram','najma','12345abcd')
+        instagram.save_credential()
+        find_credential = None
+        for credential in Credential.user_credential_list:
+            find_credential =Credential.find_by_account_name(credential.account_name)
+            return pyperclip.copy(find_credential.password)
+        Credential.copy_credential(self.new_credential.account_name)
+        self.assertEqual('12345abcd',pyperclip.paste())
+        print(pyperclip.paste())
+        
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
+
